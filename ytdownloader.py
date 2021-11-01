e45bbf50b0efc17f55eae0276fe4d427da5c8622
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2021-11-01 19:28:37 +0800

    fix

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,16 +1,18 @@
 from pytube import YouTube
 
 # modify the path where you want the videos to be saved
-PATH = "D:\pytube"
+PATH = "D:\pytube\downloads"
 
 # replace the txt file depending on your choice
 LINKS=open("D:\pytube\downloadlink.txt", "r")
 
+print("Starting download. Please wait.")
+
 for link in LINKS:  
-    print(" - downloaded ", link )
     try:
         yt = YouTube(link)
         yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(output_path = PATH)
+        print("\n", yt.streams.first().default_filename, " saved to ", PATH )
        
     except:
         print("error downloading", link)
